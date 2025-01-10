490cc6f53e0e66a06491c13d829db0dcc1e3ece5
LineXic <linexic@yeah.net>
2025-01-10 23:52:53 +0800

    2

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'LineXic';
-export const SITE_DESCRIPTION = '欢迎来到我的个人站';
+export const SITE_DESCRIPTION = 'LineXic个人站使用astro搭建用来展示自己的个人信息之类的';
